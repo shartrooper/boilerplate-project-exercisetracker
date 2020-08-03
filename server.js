36f4a6e55adb6a85cbea6e950ba1b033285307ad
Marko <michinaux.marko@gmail.com>
2020-08-03 08:04:50 -0400

    Changes Server.js and minor .gitignore addition

--- server.js
+++ server.js
@@ -1,48 +1,68 @@
 const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+    const app = express()
+    const bodyParser = require('body-parser')
 
-const cors = require('cors')
+    const cors = require('cors')
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+    const mongoose = require('mongoose')
 
+    /** this project needs a db !! **/
+    console.log('connecting to DB URI');
+
+const serverConnected = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('connected to mongo DataBase');
+    } catch (error) {
+        console.log('error connection to MongoDB:', error.message);
+    }
+};
+
+serverConnected();
 app.use(cors())
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+        extended: false
+    }))
 app.use(bodyParser.json())
-
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html')
 });
-
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
+    return next({
+        status: 404,
+        message: 'not found'
+    })
 })
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
-  let errCode, errMessage
+    let errCode,
+    errMessage
 
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
+    if (err.errors) {
+        // mongoose validation error
+        errCode = 400 // bad request
+            const keys = Object.keys(err.errors)
+            // report the first validation error
+            errMessage = err.errors[keys[0]].message
+    } else {
+        // generic or custom error
+        errCode = err.status || 500
+            errMessage = err.message || 'Internal Server Error'
+    }
+    res.status(errCode).type('txt')
     .send(errMessage)
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+    console.log('Your app is listening on port ' + listener.address().port)
 })
